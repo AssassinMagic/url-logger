c0967df68be1f6eea4711c55d1f2ea4105d4c871
23rzhao18 <raymond.j.zhao@gmail.com>
2025-01-27 23:21:34 -0600

    commit 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 'use client'
 
 import { useEffect } from "react";
-import { useRouter } from "next/router";
 
 export default function Home() {
-  const router = useRouter();
-
   useEffect(() => {
     const logAndRedirect = async () => {
       try {
